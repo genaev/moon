a88055fba83a26491a84a087b3e89d0616cac006
Misha Genaev <misha.genaev@gmail.com>
2018-04-10 18:15:49 +0700

    .

--- collect_subscribers_data.py
+++ collect_subscribers_data.py
@@ -7,7 +7,7 @@ from time import sleep
 import os.path
 
 # set the delay between requests
-wait_time = 2
+wait_time = 1
 #data_file = 'data.csv'
 out_dir = "data"
 
@@ -22,9 +22,14 @@ def download_by_coin(coin,out_dir,rewrite):
     print(coin," reddit",l.reddit, " twitter",l.twitter)
     # reddit data downloading
     if l.reddit != None:
+        rd = RedditData(l.reddit)
         if (os.path.isfile(out_dir + '/' + coin + '.reddit.csv') is not True or rewrite is True):
-            rd = RedditData(l.reddit)
             to_csv(rd.get,out_dir + '/' + coin + '.reddit.csv')
+        elif (os.path.isfile(out_dir + '/' + coin + '.reddit.csv') is True):
+            #df = pd.read_csv()
+        else:
+            to_csv(rd.get,out_dir + '/' + coin + '.reddit.csv')
+            
     # twitter data downloading
     if l.twitter != None:
         if (os.path.isfile(out_dir + '/' + coin + '.twitter.csv') is not True or rewrite is True):
@@ -35,8 +40,8 @@ def download_by_coin(coin,out_dir,rewrite):
             to_csv(tw.get, out_dir + '/' + coin + '.twitter.csv')
 
 # download data for several coin
-#download_by_coin("ethereum",out_dir,True)
-#exit(0)
+download_by_coin("eccoin",out_dir,True)
+exit(0)
 
 # download data for all coins
 i = 0
