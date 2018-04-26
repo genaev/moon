a905584a77106f3ea27ad6f78301b6a4027da145
koctu <kkoctu@gmail.com>
2018-04-26 20:18:06 +0700

    bug fixed

--- collect_coindar_data.py
+++ collect_coindar_data.py
@@ -31,7 +31,8 @@ def download_and_clust(url,csv_file,n_clust):
     tfidf_matrix = tfidf_vectorizer.fit_transform(events) #fit the vectorizer to synopses
     print('Shape is ',tfidf_matrix.shape)
     if n_clust==0:
-        n_clust = tfidf_matrix.shape[1]
+        #n_clust = tfidf_matrix.shape[1]
+        n_clust = 9
     km = KMeans(n_clusters=n_clust)
     km.fit(tfidf_matrix)
     df['cluster'] = km.labels_.tolist()
@@ -57,7 +58,9 @@ result = pd.merge(event_df, coins_df, how='inner', on=['coin_name','coin_symbol'
 #result = pd.merge(event_df, coins_df, how='inner', on=['coin_symbol'])
 result = result.drop(result.columns[[0]],axis=1)
 grouped = result.groupby('coin_id')
-print("KMeans results:\n",result.cluster.value_counts(normalize=True))
+#print("KMeans results:\n",result.cluster.value_counts(normalize=True))
+result.cluster = result.cluster.map({k: v for k, v in zip(result.cluster.value_counts(normalize=True).index, range(9))})
+#print("KMeans results:\n",result.cluster.value_counts(normalize=True))
 print(len(grouped)," coins were found in the CoinDar events")
 for name, group in grouped:
     group = group.reset_index(drop=True)
