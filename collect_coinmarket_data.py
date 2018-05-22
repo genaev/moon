4440eef38f1a8613c17dc93c7c631905158db5be
Misha Genaev <misha.genaev@gmail.com>
2018-05-22 15:49:48 +0700

    bug fixed

--- collect_coinmarket_data.py
+++ collect_coinmarket_data.py
@@ -11,22 +11,25 @@ wait_time = 2
 
 def get_html(url):
     r = requests.get(url)
-    return r.text
+    if r.status_code == 200:
+        return r.text
+    return None
 
 def main(url, name, csv_file):
     html = get_html(url)
-    soup = BeautifulSoup(html, 'lxml')
-    trs = soup.find('table').find_all('tr')
-    data = []
+    if html is not None:
+        soup = BeautifulSoup(html, 'lxml')
+        trs = soup.find('table').find_all('tr')
+        data = []
 
-    for tr in trs[1:]:
-        a = list(map(lambda x: x.string.replace(',', ''), tr.find_all('td')))
-        data.append(a)
+        for tr in trs[1:]:
+            a = list(map(lambda x: x.string.replace(',', ''), tr.find_all('td')))
+            data.append(a)
 
-    if (data is not None and len(data[0])==7):
-        df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap'])
-        df['Date'] = pd.to_datetime(df['Date'])
-        df.to_csv(csv_file)
+        if (data is not None and len(data[0])==7):
+            df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap'])
+            df['Date'] = pd.to_datetime(df['Date'])
+            df.to_csv(csv_file)
 
 i = 0
 for coin in CoinsList().get['coin_id'].tolist():
